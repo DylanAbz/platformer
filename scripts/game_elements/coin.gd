35ce5ed1efa9ea3e961be50a4e06c6a9888ca11b
Louise <louisemendiburu@gmail.com>
2026-06-24 15:19:20 +0200

    add coins to level 1

---
+++ scripts/game_elements/coin.gd
@@ -0,0 +1,11 @@
+extends Area3D
+
+@export var rotation_speed := 180.0
+
+func _process(delta):
+	rotate_y(deg_to_rad(rotation_speed * delta))
+
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		queue_free()
