62d0f703a3442b3c9f9d148bc9ff2f2ece03475f
abbouizd <dylanabbouize@gmail.com>
2026-06-24 14:40:19 +0200

    fin premier niveau

--- assets/maps/level/moving_platform.gd
+++ assets/maps/level/moving_platform.gd
@@ -1,17 +1,16 @@
 extends AnimatableBody3D
 
-@export var vitesse  : float = 3.0
-@export var distance : float = 6.0
-@export var reversed : bool = false
+@export var vitesse   : float = 3.0
+@export var distance  : float = 6.0
+@export var reversed  : bool = false
 
-var _origine    : Vector3
-var _direction  : float = 1.0
-var _reversedValue : float = 1.0
+var _origine   : Vector3
+var _direction : float = 1.0
 
 func _ready() -> void:
 	_origine = global_position
 	if reversed:
-		_reversedValue = -1.0
+		_direction = -1.0  # juste la direction de départ
 
 func _physics_process(delta: float) -> void:
 	global_position.x += vitesse * _direction * delta
@@ -19,8 +18,8 @@ func _physics_process(delta: float) -> void:
 	var d := global_position.x - _origine.x
 
 	if d > distance:
-		global_position.x = _origine.x + distance  # recale à la borne +
-		_direction = -1.0 * _reversedValue                          # force vers -Z
+		global_position.x = _origine.x + distance
+		_direction = -1.0
 	elif d < -distance:
-		global_position.x = _origine.x - distance  # recale à la borne -
-		_direction = 1.0 * _reversedValue                         # force vers +Z
+		global_position.x = _origine.x - distance
+		_direction = 1.0
